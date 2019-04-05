9e65321a2276932febf46bb1badfcafb38bdee55
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2019-04-05 15:28:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# nocesp
+
+The noc-emu single-threaded version.
+Show the message exchange with LEDs
---
+++ main/main.c
@@ -0,0 +1,9 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+static const char* TAG = "MAIN";
+
+void app_main()
+{
+    //xTaskCreate(Button::task, "Button_task", 2048, nullptr, 11, nullptr);   // Updates the Button status.
+}
